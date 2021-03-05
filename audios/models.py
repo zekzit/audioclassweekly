25ae1adb2741f7e1d874cafe3557d5983887a422
s.chanpong <ferin.mac@gmail.com>
2021-03-04 21:05:04 -0800

    change upload name

--- audios/models.py
+++ audios/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 # Create your models here.
 class AudioClass(models.Model):
@@ -11,10 +12,19 @@ class AudioClass(models.Model):
     def __str__(self):
         return self.audio_class_name
 
+def path_and_rename(path):
+    def wrapper(instance, filename):
+        ext = filename.split('.')[-1]
+        # get filename
+        filename = '{}-{}.{}'.format(instance.audio_class.audio_class_name, instance.week_name, ext)
+        # return the whole path to the file
+        return os.path.join(path, filename)
+    return wrapper
+
 class AudioClassWeek(models.Model):
     audio_class = models.ForeignKey(AudioClass, on_delete=models.CASCADE)
     week_name = models.CharField(max_length=200)
-    audio_file = models.FileField(upload_to='uploads/audios', default='', blank=True, null=True)
+    audio_file = models.FileField(upload_to=path_and_rename('audios'), default='', blank=True, null=True)
 
     class Meta:
         verbose_name = 'Audio class week'
